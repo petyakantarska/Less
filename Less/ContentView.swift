3f94e9140ec73316005391d88905bbad8ad3c29a
Petya Kantarska <petya.kantarska@icloud.com>
2023-08-01 12:17:44 +0300

    Initial Commit

---
+++ Less/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Less
+//
+//  Created by Petya Kantarska on 1.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Less/LessApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessApp.swift
+//  Less
+//
+//  Created by Petya Kantarska on 1.08.23.
+//
+
+import SwiftUI
+
+@main
+struct LessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
